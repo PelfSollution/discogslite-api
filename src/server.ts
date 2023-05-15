fbd19a79966eb03392accb5edcb218122da9a2f1
David A <daayros@protonmail.com>
2023-05-15 20:55:38 +0200

    add server.ts básico + env.sample

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", async (req, res) => {
+  res.status(200).send("hola!");
+});
+
+const { SERVER_PORT } = process.env;
+app.listen(SERVER_PORT, () => {
+  console.log(`Discogs API listening on:${SERVER_PORT}!!`);
+});
